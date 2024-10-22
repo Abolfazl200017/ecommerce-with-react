1850a0a7e30831eec2a408716f90fbdeaa8fa98e
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-10-22 14:10:08 +0330

    config new routes ruls

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,24 @@ import { Home } from './screens/home'
 import * as React from "react"
 import FullPageSpinner from './components/full-page-loading';
 import { Header } from './components/header'
+import SingleProduct from './screens/single-product';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <React.Suspense fallback={<FullPageSpinner />}>
       <Header />
-      <Home />
+      <AppRoutes />
     </React.Suspense>
   )
+}
+
+function AppRoutes() {
+  return <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route path="/single" element={<SingleProduct />} />
+    <Route path="*" element={<div>this is 404 error in future it will be more beautiful</div>} />
+  </Routes>
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,20 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import App from './App'
 import Login from './screens/login'
 import { AuthProvider } from './context/auth-context'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (<App />)
-  },
-  {
-    path: "/login",
-    element: (<Login />)
-  },
-  {
-    path: "*",
-    element: <div>
-      this is 404 error in future it will be more beautiful
-    </div>
-  }
-]);
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <RouterProvider router={router} />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </Router>
   </AuthProvider>
 )
 // <StrictMode>
