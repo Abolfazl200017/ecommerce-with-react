12ee60efef8e2854916cac86480030cd45c70c78
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-10-22 16:51:13 +0330

    config get card products on basket page

--- src/screens/basket.jsx
+++ src/screens/basket.jsx
@@ -1,9 +1,13 @@
 import FullPageSpinner from '../components/full-page-loading'
 import { useAuth } from '../context/auth-context'
 import * as React from "react"
+import { readCard, addProduct } from '../test/data/basket'
+import client from '../utils/api-client'
 
 function Basket() {
     const { status, user } = useAuth()
+    const [products, setProducts] = React.useState([])
+    const card = (readCard())
 
     React.useEffect(() => {
         if (status === 'resolved' && !user) {
@@ -12,11 +16,15 @@ function Basket() {
         }
     }, [status, user])
 
-    if (status === 'idle' || status === 'pending')
+    React.useEffect(() => {
+        client('products').then(setProducts)
+    }, [])
+
+    if (status === 'idle' || status === 'pending' || !products.length)
         return <FullPageSpinner />
 
-    return <div>
-        basket
+    return <div className='container mx-auto border rounded mt-5'>
+        {JSON.stringify(card)}
     </div>
 }
 
