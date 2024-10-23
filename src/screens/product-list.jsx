da0e2cc8a30e43ca8eaceed04c81fd1f5a6f3b94
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-10-23 11:32:57 +0330

    config card skeleton for product list page

--- src/screens/product-list.jsx
+++ src/screens/product-list.jsx
@@ -1,17 +1,22 @@
 import * as React from "react";
-import FullPageSpinner from "../components/full-page-loading";
 import client from "../utils/api-client";
 import Card from '../components/card'
+import CardSkeleton from "../components/skeleton/card-skeleton";
 
 function ProductList() {
     const [products, setProducts] = React.useState(null)
-
     React.useEffect(() => {
         client('products').then(setProducts)
     }, [])
 
     if (!products)
-        return <FullPageSpinner />
+        return (<div className='row overflow-x-hidden container-lg vw-100 pt-5' >
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((n) => (
+                <div key={n} className='col-12 col-md-4 col-lg-3 flex-nowrap mt-3' >
+                    <CardSkeleton />
+                </div>
+            ))}
+        </div>)
 
     return <div className='row overflow-x-hidden container-lg pt-5' >
         {products.map((j, index) => {
