ea80d45067f4c59d3e190361f0bc735bbf84c093
Abolfazl200017 <zeraatkar.abolfazl@yahoo.com>
2024-10-23 10:22:33 +0330

    config change number of products in card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,20 +3,27 @@ import PropTypes from 'prop-types';
 import star from '../assets/images/star.png'
 import add from '../assets/images/add-to-cart.png'
 import { useAuth } from '../context/auth-context';
-import { readCard, addProduct } from '../test/data/basket'
+import { readCard, addProduct, decreaseProductQuantity } from '../test/data/basket'
 import * as React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 
 function ProductCard({ jewelery, customStyle }) {
-    const { title, image, rating } = jewelery
+    const { title, image, rating, id } = jewelery
     const { color, isList } = customStyle
     const { user, status } = useAuth()
     const card = readCard()
-    const [isExist, setIsExist] = React.useState(() => Boolean(card[jewelery.id]))
+    const [numberInCard, setNumberInCard] = React.useState(() => card[id] ? card[id].quantity : 0)
+
+
     const addToCard = React.useCallback(() => {
-        setIsExist(true)
-        addProduct({ id: jewelery.id })
-    }, [])
+        setNumberInCard(numberInCard + 1)
+        addProduct({ id: id })
+    }, [id, numberInCard])
+
+    const decreaseNumberInCard = React.useCallback(() => {
+        setNumberInCard(numberInCard - 1)
+        decreaseProductQuantity({ id: id })
+    }, [id, numberInCard])
 
     return <Card
         style={{
@@ -48,11 +55,15 @@ function ProductCard({ jewelery, customStyle }) {
                         {rating.count}
                     </span>
                 </div>
-                <div className='w-100 d-flex justify-content-between px-3 align-items-center'>
+                <div className='w-100 d-flex justify-content-between px-3 pt-3 align-items-center'>
                     <span className='d-flex align-items-center pe-3'>
-                        {jewelery.price}$
+                        {jewelery.price * numberInCard}$
                     </span>
-                    {status === 'idle' || status === 'pending' ? <Spinner /> : !user ? '' : isExist ? 'in basket' : <Button color='warning' outline className='rounded-circle z-10' onClick={addToCard} >
+                    {status === 'idle' || status === 'pending' ? <Spinner /> : !user ? '' : numberInCard > 0 ? <div className='d-flex align-items-center w-100 justify-content-center'>
+                        <Button onClick={decreaseNumberInCard} className='text-danger bg-white fw-bold fs-lg p-0 rounded-circle btn-outline-danger' style={{ width: '30px', height: '30px' }}>-</Button>
+                        <div className='px-1 user-select-none'>{numberInCard}</div>
+                        <Button onClick={addToCard} className='text-success bg-white fw-bold fs-lg p-0 rounded-circle btn-outline-success' style={{ width: '30px', height: '30px' }}>+</Button>
+                    </div> : <Button color='warning' outline className='rounded-circle z-10' onClick={addToCard} >
                         <img style={{ width: '25px', height: '25px' }} src={add} />
                     </Button>}
                 </div>
